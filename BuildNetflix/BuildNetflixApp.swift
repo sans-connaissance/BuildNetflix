11af0d3430a7731809dfaf7a9ff19c436458dece
sans-connaissance <davemalicke@gmail.com>
2022-01-03 21:41:22 -0500

    Initial Commit

---
+++ BuildNetflix/BuildNetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildNetflixApp.swift
+//  BuildNetflix
+//
+//  Created by David Malicke on 1/3/22.
+//
+
+import SwiftUI
+
+@main
+struct BuildNetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
