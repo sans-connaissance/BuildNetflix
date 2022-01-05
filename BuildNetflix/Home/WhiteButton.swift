eaa7c70e8e6827ccbc0c24dc64f5f9793ee31b2d
sans-connaissance <davemalicke@gmail.com>
2022-01-05 11:43:11 -0500

    ADD WhiteButton properties and ui

--- BuildNetflix/Home/WhiteButton.swift
+++ BuildNetflix/Home/WhiteButton.swift
@@ -8,13 +8,43 @@
 import SwiftUI
 
 struct WhiteButton: View {
+    var text: String
+    var isOnImage: String
+    var isOffImage: String
+    var isOn: Bool
+    
+    var imageName: String {
+        if isOn {
+            return isOnImage
+        } else {
+            return isOffImage
+        }
+    }
+    
+    
+    
+    var action: () -> Void
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            action()
+        } label: {
+            HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.white)
+                Text(text)
+                    .foregroundColor(.white)
+                    .font(.system(size: 16))
+                    .bold()
+
+            }
+        }
     }
 }
 
 struct WhiteButton_Previews: PreviewProvider {
     static var previews: some View {
-        WhiteButton()
+        WhiteButton(text: "Play", isOnImage: "triangle", isOffImage: "square", isOn: true) {
+            // to add
+        }
     }
 }
