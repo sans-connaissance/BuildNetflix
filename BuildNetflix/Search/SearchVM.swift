f39bbd960bd24124a5573429e0cb2a6c82184958
sans-connaissance <davemalicke@gmail.com>
2022-01-11 21:20:25 -0500

    ADD SearchVM

--- BuildNetflix/Search/SearchVM.swift
+++ BuildNetflix/Search/SearchVM.swift
@@ -23,17 +23,32 @@ class SearchVM: ObservableObject {
     public func updateSearchText(with text: String) {
         setViewState(to: .loading)
         
-        getSearchResults(forText: text)
+        if text.count > 0 {
+            isShowingPopularMovies = false
+            getSearchResults(forText: text)
+        }
     }
     
     
     private func getPopularMovies() {
-        
+        popularMovies = generateMovies(40)
         
     }
     
     private func getSearchResults(forText text: String) {
         
+        let haveResult = Int.random(in: 0...3)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            if haveResult == 0 {
+                self.searchResults = []
+                self.setViewState(to: .empty)
+            } else {
+                let movies = generateMovies(21)
+                self.searchResults = movies
+                self.setViewState(to: .ready)
+            }
+        }
     }
     
     private func setViewState(to state: ViewState) {
